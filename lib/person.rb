4fefca7e2c461d2809dcca5504b93d6580695ba4
Tim Quimbo <tquimbo1@gmail.com>
2021-03-06 02:26:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,16 +4,13 @@ def initialize(name)
 @name = name
 end
 
-def breed=(breed)
-  @breed = breed
-end
 
 end
 
 class Dog
 
-def initialize(breed)
-  @breed = breed
+def initialize(name)
+  @name = name
 end
 
 end
